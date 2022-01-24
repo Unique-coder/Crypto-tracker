e7f8b3db3385f92b7f6fd8b41d691dd173181f23
Unique-coder <tom-lesh@outlook.com>
2022-01-24 15:45:06 +0100

    First newutral commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+import "./App.css";
+import Coin from "./Coin";
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  const fetchCoin = async () => {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+    );
+    const coinData = await res.json();
+    setCoins(coinData);
+    console.log(coinData);
+    return coinData;
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input
+            className="coin-input"
+            type="text"
+            onChange={handleChange}
+            placeholder="Search"
+          />
+        </form>
+      </div>
+      {filteredCoins.map((coin) => {
+        return <Coin coin={coin} key={coin.id} />;
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Coin.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Coin.css";
+
+const Coin = ({ coin }) => {
+  const {
+    name,
+    current_price,
+    symbol,
+    total_volume,
+    market_cap,
+    image,
+    price_change_percentage_24h,
+  } = coin;
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" />
+          <h1>{name}</h1>
+          <p className="coin-symbol">{symbol}</p>
+        </div>
+        <div className="coin-data">
+          <p className="coin-price">${current_price}</p>
+          <p className="coin-volume">${total_volume.toLocaleString()}</p>
+
+          {price_change_percentage_24h < 0 ? (
+            <p className="coin-percent red">
+              {price_change_percentage_24h.toFixed(2)}%
+            </p>
+          ) : (
+            <p className="coin-percent green">
+              {price_change_percentage_24h.toFixed(2)}%
+            </p>
+          )}
+          <p className="coin-marketcap">
+            Mkt Cap: ${market_cap.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
